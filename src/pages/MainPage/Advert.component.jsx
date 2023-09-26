4337992ce2ebff26630550a9898317d191b3e4fc
Sandro-Gabunia <gabuniasandro99@gmail.com>
2023-09-26 23:18:46 +0400

    ReadMe file is updated and Advert.Component is corrected

--- src/pages/MainPage/Advert.component.jsx
+++ src/pages/MainPage/Advert.component.jsx
@@ -1,13 +1,10 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-// import './styles.css';
 
-// import required modules
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 
 const advertData = [
